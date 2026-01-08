b4c8a7fddb5ce079d1b72bd6cac34365e3261453
tsuji-yuhi <tsujiyuhi@gmail.com>
2026-01-08 22:33:46 +0900

    [Red] Add test case to verify side effects of Dollar#times

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -6,7 +6,13 @@ require 'dollar'
 RSpec.describe 'Dollar' do
   it 'multiplication' do
     five = Dollar.new(5)
+
+    # 1回目の掛け算：2 * 5 = 10 を期待
     five.times(2)
     expect(five.amount).to eq 10
+
+    # 2回目の掛け算：元の five は 5 のままであり、 5 * 3 = 15 を期待
+    five.times(3)
+    expect(five.amount).to eq 15
   end
 end
