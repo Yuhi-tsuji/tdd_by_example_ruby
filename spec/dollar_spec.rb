dc04f6387f78c29dedcc2c9fa37bca435cd96716
tsuji-yuhi <tsujiyuhi@gmail.com>
2026-01-08 22:35:00 +0900

    [Red] Update Dollar#times to return a new Dollar object in test

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
-# spec/dollar_spec.rb
 require 'dollar'
 
 RSpec.describe 'Dollar' do
   it 'multiplication' do
     five = Dollar.new(5)
 
-    # 1回目の掛け算：2 * 5 = 10 を期待
-    five.times(2)
-    expect(five.amount).to eq 10
+    # timesメソッドが新しいオブジェクトを返すと定義する
+    product = five.times(2)
+    expect(product.amount).to eq 10
 
-    # 2回目の掛け算：元の five は 5 のままであり、 5 * 3 = 15 を期待
-    five.times(3)
-    expect(five.amount).to eq 15
+    # 元の five オブジェクトは 5ドルのままであることを期待する
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
